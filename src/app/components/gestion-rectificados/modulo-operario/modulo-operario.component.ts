cae2b0351e599b33cde25aabd61b788b0ee57b82
lautarocabral <lautacabral96@gmail.com>
2023-12-19 12:43:51 -0300

    Alta modulo de operarios modal y form validator y archivos vs para debug

--- src/app/components/gestion-rectificados/modulo-operario/modulo-operario.component.ts
+++ src/app/components/gestion-rectificados/modulo-operario/modulo-operario.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Rectificado } from 'src/app/models/rectificado.model';
 import { RectificadosService } from 'src/app/services/rectificado/rectificados.service';
 
@@ -9,25 +12,61 @@ import { RectificadosService } from 'src/app/services/rectificado/rectificados.s
   styleUrls: ['./modulo-operario.component.scss']
 })
 export class ModuloOperarioComponent {
-  rectificados: Rectificado[] = [];
+  operarios: any[] = [];
+  operarioForm!: FormGroup;
+  @ViewChild('addOperarioModal') addOperarioModal!: ElementRef;
 
-  constructor(private router: Router, private rectificadosService: RectificadosService) { }
+  constructor( private rectificadosService: RectificadosService, private fb: FormBuilder,
+    private datePipe: DatePipe) { }
 
   ngOnInit() {
-    this.getRectificadosList();
+    this.getOperariosList();
+    this.initForm();
   }
 
-  navigateToUpdateMotor() {
-    this.router.navigate(['/updateMotor']);
+  initForm() {
+    this.operarioForm = this.fb.group({
+      nombre: new FormControl(null, Validators.required),
+      apellido: new FormControl(null, Validators.required),
+      dni: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(9),
+        Validators.pattern(/^\d+$/)
+      ]),
+      fechaIngreso: new FormControl(null, Validators.required)
+    });
   }
 
-  getRectificadosList() {
+  editOperario() {
+
+  }
+
+  addOperario(body: any) {
     try {
-      this.rectificadosService.getAllRectificados().subscribe({
+      this.rectificadosService.addRectificado(body).subscribe({
+        next: (response) => {
+          this.operarioForm.reset();
+          this.closeModal();
+          this.getOperariosList();
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
+    } catch (error) { }
+  }
+  deleteOperario() {
+
+  }
+
+  getOperariosList() {
+    try {
+      this.rectificadosService.getAllOperarios().subscribe({
         next: (response) => {
           console.log(response);
-          
-          this.rectificados = response;
+
+          this.operarios = response;
         },
         error: (error) => {
           // Handle error here
@@ -37,4 +76,34 @@ export class ModuloOperarioComponent {
 
     }
   }
+
+  onSubmit() {
+    var body = {
+      nombre: this.operarioForm.value.nombre,
+      apellido: this.operarioForm.value.apellido,
+      dni: this.operarioForm.value.dni,
+      fecha: this.operarioForm.value.fechaIngreso,
+    };
+    this.addOperario(body);
+    this.closeModal();
+  }
+
+
+  closeModal() {
+    const modalElement = this.addOperarioModal.nativeElement;
+    modalElement.classList.remove('show');
+    modalElement.setAttribute('aria-hidden', 'true');
+    modalElement.style.display = 'none';
+
+
+    const modalBackdrop = document.querySelector('.modal-backdrop');
+    if (modalBackdrop && modalBackdrop.parentNode) {
+      modalBackdrop.parentNode.removeChild(modalBackdrop);
+    }
+
+    // para resetear el scroll
+    // document.body.classList.remove('modal-open');
+    // document.body.style.paddingRight = '';
+  }
+
 }
